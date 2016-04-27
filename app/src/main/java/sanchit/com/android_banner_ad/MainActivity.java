99b32e30a6f13b31828b91f287f9efd97fa3ab0b
Sanchit Goel <sanchit.mgoel007@gmail.com>
2016-04-27 13:49:46 +0530

    Interstitial Ads + BAnner

--- app/src/main/java/sanchit/com/android_banner_ad/MainActivity.java
+++ app/src/main/java/sanchit/com/android_banner_ad/MainActivity.java
@@ -6,6 +6,7 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
+    private InterstitialAd interstitial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
         AdView mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+        
+         AdRequest adRequest = new AdRequest.Builder().build();
+        
+        // Prepare the Interstitial Ad
+        interstitial = new InterstitialAd(MainActivity.this);
+        // Insert the Ad Unit ID
+        interstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
+ 
+        interstitial.loadAd(adRequest);
+        // Prepare an Interstitial Ad Listener
+        interstitial.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+        // Call displayInterstitial() function
+                displayInterstitial();
+            }
     }
+    public void displayInterstitial() {
+      // If Ads are loaded, show Interstitial else show nothing.
+       if (interstitial.isLoaded()) {
+       interstitial.show();
+      }
+}
+ 
 }
